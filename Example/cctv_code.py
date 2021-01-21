aeacee93c3d17f1b8004c9f6c4429fafa5c6395a
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-01-21 23:53:46 +0700

    Create cctv_code.py

---
+++ Example/cctv_code.py
@@ -0,0 +1,16 @@
+from justgood import imjustgood
+
+media = imjustgood("YOUR_APIKEY_HERE")
+data = media.cctv_code()
+
+# Get attributes
+result += "CCTV Available"
+for a in data["result"]["available"]:
+    result += "\n{} {}".format(a,data["result"]["available"][a])
+result += "\n\nCCTV Active"
+for b in data["result"]["active"]:
+    result += "\n{} {}".format(a,data["result"]["active"][a])
+print(result)
+
+# Get JSON results
+print(data)
