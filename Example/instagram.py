56beb9c2a996c62baa3a7d1f6df675c3d1339176
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-01-21 14:57:02 +0700

    Update instagram.py

--- Example/instagram.py
+++ Example/instagram.py
@@ -4,11 +4,11 @@ media = imjustgood("YOUR_APIKEY_HERE")
 userId = "the.autobots_corp" # example instagram id
 data = media.instagram(userId)
 
+# Get certain attributes
 if data["result"]["private"] == False:
    private = "Disable"
 else:
    private = "Enable"
-
 result = "Instagram Profile"
 result += "\nUsername : {}".format(data["result"]["username"])
 result += "\nFullname : {}".format(data["result"]["fullname"])
@@ -19,7 +19,6 @@ result += "\nFollower : {}".format(data["result"]["follower"])
 result += "\nFollowing : {}".format(data["result"]["following"])
 result += "\n\nPicture :\n{}".format(data["result"]["picture"])
 result += "\n\nProfile :\n{}".format(data["result"]["profile"])
-
 if data["result"]["lastpost"] != []:
     number = 0
     result += "\n\nLastpost"
@@ -27,5 +26,7 @@ if data["result"]["lastpost"] != []:
         number += 1
         result += "\n{}. {} ( {} )".format(number, a["url"], a["created"])
         result += "\n    Page : {}".format(a["page"])
-
 print(result)
+
+# Get JSON result
+print(data)
