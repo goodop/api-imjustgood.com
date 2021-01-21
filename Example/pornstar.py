db155e1da77d99d50446b808efe7407a250ae467
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-01-21 17:11:49 +0700

    Create pornstar.py

---
+++ Example/pornstar.py
@@ -0,0 +1,20 @@
+from justgood import imjustgood
+import random
+
+media = imjustgood("YOUR_APIKEY_HERE")
+data = media.pornstar()
+
+# Get attributes
+stars = random.choice(data["result"])
+result = "Pornstar"
+result += "\nName : {}".format(stars["pornstar"])
+result += "\nBirth : {}".format(stars["birth"])
+result += "\nCountry : {}".format(stars["country"])
+result += "\nHeight : {}".format(stars["height"])
+result += "\nBreast : {}".format(stars["breast"])
+result += "\nTits : {}".format(stars["tits"])
+result += "\n\nImage URL :\n{}".format(data["result"]["image"])
+print(result)
+
+# Get JSON results
+print(data)
