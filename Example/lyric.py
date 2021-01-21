a7ce80f954c566c9191787b52d64d82620b8d0ea
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-01-21 15:02:06 +0700

    Update lyric.py

--- Example/lyric.py
+++ Example/lyric.py
@@ -4,9 +4,12 @@ media = imjustgood("YOUR_APIKEY_HERE")
 query = "crawling" # example query
 data = media.lyric(query)
 
+# Get attributes
 result = "Lyric Finder"
 result += "\nTitle : {}".format(data["result"]["title"])
 result += "\nArtist : {}".format(data["result"]["artist"])
 result += "\n\Lyric :\n{}".format(data["result"]["lyric"])
-
 print(result)
+
+# Get JSON results
+print(data)
