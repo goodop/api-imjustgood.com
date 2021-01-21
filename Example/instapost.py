1f2f80f57e28467930e70525fcefc99b0eb07781
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-01-21 12:12:28 +0700

    Update instapost.py

--- Example/instapost.py
+++ Example/instapost.py
@@ -1,2 +1,21 @@
-# -*- coding: utf-8 -*-
 from justgood import imjustgood
+
+media = imjustgood("YOUR_APIKEY_HERE")
+post_url = "https://instagram.com/p/CJtqfEbhpjO/"
+data = media.instapost(post_url)
+number = 0
+result = "Instagram Post"
+result += "\nUsername : {}".format(data["result"]["username"])
+result += "\nFullname : {}".format(data["result"]["fullname"])
+result += "\nCreated : {}".format(data["result"]["created"])
+result += "\nCaption : {}".format(data["result"]["caption"])
+result += "\n\nPicture :\n{}".format(data["result"]["picture"])
+result += "\n\nMedia Post :\n{}".format(data["result"]["picture"])
+for a in data["result"]["postData"]:
+    number += 1
+    if a["type"] == "image":
+       result += "\n{}. Image Url : {}".format(number, a["postUrl"])
+    if a["type"] == "video":
+       result += "\n{}. Video Url : {}".format(number, a["postUrl"])
+       result += "\n    Video Poster : {}".format(number, a["poster"])
+print(result)
