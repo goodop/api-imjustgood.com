12a732ffa8e77ad302485cab2ea91382788aa1a0
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-01-21 13:38:06 +0700

    Create smule.py

---
+++ Example/smule.py
@@ -0,0 +1,46 @@
+from justgood import imjustgood
+
+media = imjustgood("YOUR_APIKEY_HERE")
+userId = "avamax"
+data = media.joox(userId)
+
+
+# -- Get Profile Attributes
+
+result = "Smule Profile"
+result += "\nUsername : {}".format(data["result"]["username"])
+result += "\nAvatar : {}".format(data["result"]["avatar"])
+
+number = 0
+result += "\n\nRecording : "
+for a in data["result"]["recording"]:
+    number += 1
+    result += "\n{}. {}".format(number, a["title"])
+total = len(data["result"]["recording"])
+result += "Total {} Recording.".format(total)
+
+print(result)
+
+
+# -- Get Recording Attributes
+
+select_ = 1
+recording = data["result"]["recording"]
+list_recording = [a for a in recording]
+track = recording[list_recording[select_-1]]
+
+result = "Title : {}".format(track["title"])
+result += "\nArtist : {}".format(track["artist"])
+result += "\nType : {}".format(track["type"])
+result += "\nPerformance : {}".format(track["performance"])
+result += "\nLoves : {}".format(track["loves"])
+result += "\nListens : {}".format(track["listens"])
+result += "\nCreated : {}".format(track["created"])
+result += "\n\nCaption :\n{}".format(track["caption"])
+result += "\n\nThumbnail :\n{}".format(track["thumbnail"])
+if track["type"] == "audio":
+    result += "\n\Mp3 :\n{}".format(track["mp3Url"])
+if track["type"] == "video":
+    result += "\n\Mp4 :\n{}".format(track["mp4Url"])
+
+print(result)
