721fbec3064b41bdc725d02dc665fb5f7edc2be7
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-01-21 15:04:46 +0700

    Update tiktok.py

--- Example/tiktok.py
+++ Example/tiktok.py
@@ -1,9 +1,10 @@
 from justgood import imjustgood
 
 media = imjustgood("YOUR_APIKEY_HERE")
-userId = "maymii69"
+userId = "maymii69" # example tiktok id
 data = media.tiktok(userId)
 
+# Get attributes
 result = "Tiktok Profile"
 result += "\nUsername : {}".format(data["result"]["username"])
 result += "\nFullname : {}".format(data["result"]["fullname"])
@@ -13,5 +14,7 @@ result += "\nFollowing : {}".format(data["result"]["following"])
 result += "\nLikes : {}".format(data["result"]["likes"])
 result += "\nPicture : {}".format(data["result"]["pictureUrl"])
 result += "\nProfile : {}".format(data["result"]["profileUrl"])
-
 print(result)
+
+# Get JSON results
+print(data)
