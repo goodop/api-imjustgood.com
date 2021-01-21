df13041dd96ea4500b8c26c7f9bd3184248e3430
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-01-21 21:30:30 +0700

    Create adzan.py

---
+++ Example/adzan.py
@@ -0,0 +1,22 @@
+from justgood import imjustgood
+
+media = imjustgood("YOUR_APIKEY_HERE")
+city = "palembang" # city name
+data = media.adzan(city)
+
+# Get attributes
+result = "Info Waktu Sholat"
+result += "\nWilayah : {}".format(data["result"]["wilayah"])
+result += "\nTanggal : {}".format(data["result"]["tanggal"])
+result += "\n\nImsyak : {}".format(data["result"]["adzan"]["imsyak"])
+result += "\nSubuh : {}".format(data["result"]["adzan"]["subuh"])
+result += "\nTerbit : {}".format(data["result"]["adzan"]["terbit"])
+result += "\nDhuha : {}".format(data["result"]["adzan"]["dhuha"])
+result += "\nDzuhur : {}".format(data["result"]["adzan"]["dzuhur"])
+result += "\nAshar : {}".format(data["result"]["adzan"]["ashar"])
+result += "\nMaghrib : {}".format(data["result"]["adzan"]["maghrib"])
+result += "\nIsya : {}".format(data["result"]["adzan"]["isya"])
+print(result)
+
+# Get JSON results
+print(data)
