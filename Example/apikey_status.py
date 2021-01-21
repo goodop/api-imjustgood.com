c41430ff0f541bcf048c753fa60c5daad451eb10
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-01-21 15:00:30 +0700

    Update apikey_status.py

--- Example/apikey_status.py
+++ Example/apikey_status.py
@@ -4,7 +4,7 @@ apikey = "YOUR_APIKEY_HERE"
 host = "https://api.imjustgood.com/status?apikey={}".format(apikey)
 data = json.loads(requests.get(host).text)
 
-# Get certain attributes
+# Get attributes
 result = "Apikey Status"
 result += "\nID : {}".format(data["result"]["id"])
 result += "\nType : {}".format(data["result"]["type"])
