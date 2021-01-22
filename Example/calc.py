ece47c49c67622817bdce4c43217176867c02cf5
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-01-22 08:59:16 +0700

    Update calc.py

--- Example/calc.py
+++ Example/calc.py
@@ -1,8 +1,7 @@
 import json, requests
 
-host = "https://api.imjustgood.com/calc="
 calculated = "10+10x10:10-10" # example multiplication
-
+host = "https://api.imjustgood.com/calc="+calculated
 data = json.loads(requests.get(host).text)
 result = data["result"]
 
