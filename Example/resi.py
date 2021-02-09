98a9de76ee5f6b06e9406786156a8fff6ee731a2
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-02-10 02:50:26 +0700

    Update resi.py

--- Example/resi.py
+++ Example/resi.py
@@ -6,23 +6,23 @@ resiNum = "CGK2H03789568816" # example resi number
 data = media.resi(courier,resiNum)
 
 # Get attributes
-result = "Info Pengiriman\n"
+result = "{}\n".format(data["result"]["courier"])
 result += "\nResi : {}".format(data["result"]["code"])
-result += "\nExpress : {}".format(data["result"]["courier"])
-result += "\nType : {}".format(data["result"]["service"])
 result += "\nStatus : {}".format(data["result"]["status"])
-result += "\nHarga : {}".format(data["result"]["price"])
+result += "\nJenis : {}".format(data["result"]["service"])
 result += "\nBerat : {}".format(data["result"]["weight"])
-result += "\nTanggal : {}".format(data["result"]["date"])
+result += "\nHarga : {}".format(data["result"]["price"])
 result += "\nPukul : {}".format(data["result"]["time"])
-result += "\n\nDeskripsi :\n{}, {}".format(data["result"]["desc"])
-result += "\n\nPengirim :\n{}, {}".format(data["result"]["sender"]["name"],data["result"]["sender"]["city"])
-result += "\n\nPenerima :\n{}, {}\n\n".format(data["result"]["receiver"]["name"],data["result"]["receiver"]["city"])
-result += "Riwayat Pengiriman :"
-for a in data["result"]["timeExpress"]:
-    result += "\n[{}] {}".format(a["date"],a["desc"])
-    if a["location"] != "":
-       result += "\n{}\n".format(a["location"])
+result += "\nTanggal : {}".format(data["result"]["date"])
+result += "\n\nDeskripsi :\n{}".format(data["result"]["desc"])
+result += "\n\nPengirim :\n{} - {}".format(data["result"]["sender"]["name"],data["result"]["sender"]["city"])
+result += "\n\nPenerima :\n{} - {}".format(data["result"]["receiver"]["name"],data["result"]["receiver"]["city"])
+if data["result"]["timeExpress"] != []:
+    for a in data["result"]["timeExpress"]:
+        result += "\n\n[ {} ]".format(a["date"])
+        result += "\n{}".format(a["desc"])
+        if a["location"] != "":
+           result += "\n{}".format(a["location"])
 print(result)
 
 # Get JSON results
