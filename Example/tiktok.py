de3fb9087f695a9983eab5cfd03146f434620bfe
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-02-12 14:11:32 +0700

    Update tiktok.py

--- Example/tiktok.py
+++ Example/tiktok.py
@@ -5,16 +5,16 @@ userId = "maymii69" # example tiktok id
 data = media.tiktok(userId)
 
 # Get attributes
-result = "Tiktok Profile"
+result = "Tiktok Profile\n"
 result += "\nUsername : {}".format(data["result"]["username"])
 result += "\nFullname : {}".format(data["result"]["fullname"])
 result += "\nBiography : {}".format(data["result"]["biography"])
 result += "\nFollowers : {}".format(data["result"]["followers"])
 result += "\nFollowing : {}".format(data["result"]["following"])
 result += "\nLikes : {}".format(data["result"]["likes"])
-result += "\nProfile : {}".format(data["result"]["profileUrl"])
-result += "\nPicture : {}".format(data["result"]["pictureUrl"])
-result += "\nLast Video : {}".format(data["result"]["lastpost"])
+result += "\n\nProfile : {}".format(data["result"]["profileUrl"])
+result += "\n\nPicture : {}".format(data["result"]["pictureUrl"])
+result += "\n\nLast Video : {}".format(data["result"]["lastpost"])
 print(result)
 
 # Get JSON results
