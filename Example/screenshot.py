7d8db63ed56836e712ed9a7cd33b0df2fe72c9da
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-02-14 14:43:51 +0700

    Update screenshot.py

--- Example/screenshot.py
+++ Example/screenshot.py
@@ -5,8 +5,9 @@ web_url = "https://imjustgood.com/team" # example website url
 data = media.screenshot(web_url)
 
 # Get attributes
-result = "Dekstop Display : {}".format(data["result"]["desktop"])
-result += "\nMobile Display : {}".format(data["result"]["mobile"])
+result = "Screenshot Result :"
+result += "\nDekstop screen : {}".format(data["result"]["desktop"])
+result += "\nMobile screen : {}".format(data["result"]["mobile"])
 print(result)
 
 # Get JSON results
