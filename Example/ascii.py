9188f69c30d778ab38da73e61e4dc1a097dc0f9d
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-02-19 18:38:49 +0700

    Update ascii.py

--- Example/ascii.py
+++ Example/ascii.py
@@ -1,6 +1,6 @@
 import json, requests
 
 query = "imjustgood" # example your string
-data = requests.get("https://api.imjustgood.com/ascii="+query).text
+data = requests.get("https://api.imjustgood.com/ascii="+query).text.split("pre")[1][1:][:-2]
 
 print(data)
