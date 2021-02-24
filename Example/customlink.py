c8bad5ce6267fcbcba42f9235be69b2e0c0cd56f
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-02-25 03:32:45 +0700

    Create customlink.py

---
+++ Example/customlink.py
@@ -0,0 +1,10 @@
+import json, requests
+
+host = "https://api.imjustgood.com/custom/make"
+url = "https://cdn-0.scatteredquotes.com/wp-content/uploads/2019/07/Sad-Quotes-2-945x945.jpg" # example your url
+label = "quotes" # example your custom name
+
+headers = {"label": label, "url": url}
+data = json.loads(requests.get(host, headers=headers).text)
+
+print(data["result"])
