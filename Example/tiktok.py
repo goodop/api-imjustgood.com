fc9c250df4ce4ba95823ff38c9d76b21808117f2
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-02-27 18:16:13 +0700

    Update tiktok.py

--- Example/tiktok.py
+++ Example/tiktok.py
@@ -12,9 +12,13 @@ result += "\nBiography : {}".format(data["result"]["biography"])
 result += "\nFollowers : {}".format(data["result"]["followers"])
 result += "\nFollowing : {}".format(data["result"]["following"])
 result += "\nLikes : {}".format(data["result"]["likes"])
+result += "\nVideos : {}".format(data["result"]["videos"])
 result += "\n\nProfile : {}".format(data["result"]["profileUrl"])
 result += "\n\nPicture : {}".format(data["result"]["pictureUrl"])
-result += "\n\nLast Video : {}".format(data["result"]["lastpost"])
+result += "\n\nLastpost :"
+result += "\n- Page : {}".format(data["result"]["lastpost"]["pageUrl"])
+result += "\n- Poster : {}".format(data["result"]["lastpost"]["poster"])
+result += "\n- Video : {}".format(data["result"]["lastpost"]["videoUrl"])
 print(result)
 
 # Get JSON results
