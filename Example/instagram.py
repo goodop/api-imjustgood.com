60d15e7659116d09cbaf918d3d9acc11a8b9607a
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-03-03 02:42:07 +0700

    Update instagram.py

--- Example/instagram.py
+++ Example/instagram.py
@@ -5,15 +5,14 @@ userId = "the.autobots_corp" # example instagram id
 data = media.instagram(userId)
 
 # Get attributes
-if data["result"]["private"] == False:
-   private = "Disable"
-else:
-   private = "Enable"
 result = "Instagram Profile"
+result += "\nUsername : {}".format(data["result"]["id"])
 result += "\nUsername : {}".format(data["result"]["username"])
 result += "\nFullname : {}".format(data["result"]["fullname"])
 result += "\nBiography : {}".format(data["result"]["biography"])
-result += "\nPrivate : {}".format(private)
+result += "\nWebsite : {}".format(data["result"]["website"])
+result += "\nPrivate : {}".format(data["result"]["private"])
+result += "\nVerified : {}".format(data["result"]["verified"])
 result += "\nPost : {}".format(data["result"]["post"])
 result += "\nFollower : {}".format(data["result"]["follower"])
 result += "\nFollowing : {}".format(data["result"]["following"])
@@ -24,8 +23,12 @@ if data["result"]["lastpost"] != []:
     result += "\n\nLastpost"
     for a in data["result"]["lastpost"]:
         number += 1
-        result += "\n{}. {} ( {} )".format(number, a["url"], a["created"])
-        result += "\n    Page : {}".format(a["page"])
+        result += "\n{}. {}".format(number, a["caption"])
+        result += "\n{}".format(a["url"])
+        result += "\n   Like : {}".format(number, a["like"])
+        result += "\n   Comment : {}".format(number, a["comment"])
+        result += "\n   Created : {}".format(a["created"])
+        result += "\n   PageUrl : {}".format(a["page"])
 print(result)
 
 # Get JSON results
