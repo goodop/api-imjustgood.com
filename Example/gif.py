ac51b67947d05ffe19c7fb7c765b1d75adfdb7f7
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-03-06 02:25:54 +0700

    Update gif.py

--- Example/gif.py
+++ Example/gif.py
@@ -2,7 +2,6 @@ from justgood import imjustgood
 
 media = imjustgood("YOUR_APIKEY_HERE")
 query = "coding" # example query
-url = "https://www.imjustgood.com" # example url
 data = media.gif(query)
 for gif in data["result"]:
     print(gif)
