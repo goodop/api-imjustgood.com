3b1942a42e901d09c8272cde17730262da0be9eb
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-03-18 22:31:16 +0700

    Update gif.py

--- Example/gif.py
+++ Example/gif.py
@@ -1,7 +1,13 @@
 from justgood import imjustgood
+import random
 
 media = imjustgood("YOUR_APIKEY_HERE")
 query = "coding" # example query
 data = media.gif(query)
-for gif in data["result"]:
-    print(gif)
+
+# Get attributes
+result = random.choice(data["result"])
+print(result)
+
+# Get JSON results
+print(data)
