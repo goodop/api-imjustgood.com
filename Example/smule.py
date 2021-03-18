d734a6290cbe0c14946fe1e7d409fe54606d6aa3
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-03-18 22:15:07 +0700

    Update smule.py

--- Example/smule.py
+++ Example/smule.py
@@ -6,35 +6,43 @@ data = media.smule(userId)
 
 # Get profile attributes
 result = "Smule Profile"
+result += "\n\nAccount Id : {}".format(data["result"]["accountId"])
 result += "\nUsername : {}".format(data["result"]["username"])
-result += "\nAvatar : {}".format(data["result"]["avatar"])
-number = 0
-result += "\n\nRecording : "
-for a in data["result"]["recording"]:
-    number += 1
-    result += "\n{}. {}".format(number, a["title"])
-total = len(data["result"]["recording"])
-result += "Total {} Recording.".format(total)
+result += "\nFullname : {}".format(data["result"]["fullname"])
+result += "\nBiography : {}".format(data["result"]["biography"])
+result += "\nLocation : {}".format(data["result"]["location"])
+result += "\nFollowers : {}".format(data["result"]["followers"])
+result += "\nFollowing : {}".format(data["result"]["following"])
+result += "\nRecording : {}".format(data["result"]["recording"])
+result += "\nVIP : {}".format(data["result"]["vip"])
+result += "\nVerified : {}".format(data["result"]["verified"])
+result += "\nProfile URL : {}".format(data["result"]["profileUrl"])
+result += "\nPicture URL : {}".format(data["result"]["pictureUrl"])
+if data["result"]["lastpost"] != []:
+    number = 0
+    result += "\n\nLastpost :"
+    for i in data["result"]["lastpost"]:
+        number += 1
+        result += "\n  {}. {}".format(number,i["title"])
 print(result)
 
-# Get recording attributes
-select_ = 1 # number of recording
-recording = data["result"]["recording"]
-list_recording = [a for a in recording]
-track = list_recording[select_-1]
-result = "Title : {}".format(track["title"])
-result += "\nArtist : {}".format(track["artist"])
-result += "\nType : {}".format(track["type"])
-result += "\nPerformance : {}".format(track["performance"])
-result += "\nLoves : {}".format(track["loves"])
-result += "\nListens : {}".format(track["listens"])
-result += "\nCreated : {}".format(track["created"])
-result += "\n\nCaption :\n{}".format(track["caption"])
-result += "\n\nThumbnail :\n{}".format(track["thumbnail"])
-if track["type"] == "audio":
-    result += "\n\Mp3 :\n{}".format(track["mp3Url"])
-if track["type"] == "video":
-    result += "\n\nMp4 :\n{}".format(track["mp4Url"])
+# Get lastpost attributes
+postcount = 1 # count number of lastpost
+lastpost = data["result"]["lastpost"][postcount-1]
+result = "Title : {}".format(lastpost["title"])
+result += "\nArtist : {}".format(lastpost["artist"])
+result += "\nType : {}".format(lastpost["type"])
+result += "\nCollabs : {}".format(lastpost["performances"])
+result += "\nCreated : {}".format(lastpost["created"])
+result += "\nListens : {}".format(lastpost["listens"])
+result += "\nLoves : {}".format(lastpost["loves"])
+result += "\nGifts : {}".format(lastpost["gifts"])
+result += "\nComments : {}".format(lastpost["comments"])
+result += "\nCaption :\n{}".format(lastpost["caption"])
+if lastpost["type"] == "video":
+    result += "\nThumbnail : {}".format(lastpost["thumbnail"])
+    result += "\nVideo URL :\n{}".format(lastpost["videoUrl"])
+result += "\nAudio URL :\n{}".format(lastpost["audioUrl"])
 print(result)
 
 # Get JSON results
