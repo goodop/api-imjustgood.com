a2ff9e5577facf2b6b417f14688ce324ade4a3d5
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-03-18 22:18:17 +0700

    Update smuledl.py

--- Example/smuledl.py
+++ Example/smuledl.py
@@ -1,18 +1,18 @@
 from justgood import imjustgood
 
 media = imjustgood("YOUR_APIKEY_HERE")
-post_url = "https://www.smule.com/p/1998769355_3429377039" # example recording url
+post_url = "https://www.smule.com/p/1998769355_3429377039" # example post url
 data = media.smuledl(query)
 
 # Get attributes
 result = "Smule Downloader"
-result += "\nTitle : {}".format(data["result"]["title"])
+result += "\n\nTitle : {}".format(data["result"]["title"])
 result += "\nType : {}".format(data["result"]["type"])
-result += "\n\nCaption :\n{}".format(data["result"]["caption"])
-result += "\n\nThumbnail :\n{}".format(data["result"]["thumbnail"])
-result += "\n\nMp3 :\n{}".format(data["result"]["mp3Url"])
+result += "\n\nCaption : {}".format(data["result"]["caption"])
 if data["result"]["type"] == "video":
-   result += "\n\nMp4 : {}".format(data["result"]["mp4Url"])
+    result += "\n\nThumbnail : {}".format(data["result"]["thumbnail"])
+    result += "\n\nVideo URL : {}".format(data["result"]["mp4Url"])
+result += "\n\nAudio URL : {}".format(data["result"]["mp3Url"])
 print(result)
 
 # Get JSON results
