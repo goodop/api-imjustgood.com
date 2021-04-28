6b593c003cbc376963254c8951308f475b30bd01
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-04-28 16:50:14 +0700

    Update langcode.py

--- Example/langcode.py
+++ Example/langcode.py
@@ -4,6 +4,6 @@ host = "https://api.imjustgood.com/language/code"
 
 data = json.loads(requests.get(host).text)
 result = "Language Code :"
-for a in data["result"]:
-    result += "\n{} ( {} )".format(a,data["result"][a])
+for a in data:
+    result += "\n{}".format(a)
 print(result)
