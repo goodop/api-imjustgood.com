22f73530f5860de4d4f1fcc37548b98de242af67
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-04-28 18:00:20 +0700

    Update line_secondary.py

--- Example/line_secondary.py
+++ Example/line_secondary.py
@@ -1,37 +1,33 @@
-from justgood import imjustgood
 import time, json, requests
 
 # Line Version Info https://api.imjustgood.com/line
 
 apikey = "YOUR_APIKEY_HERE"
+cert = None # if you have a cert code put here
 appname = "CHROMEOS\t2.4.4\tChromeOS\t88" # example app name
 sysname = "JUSTGOOD" # example system name
-cert = None # if you have a cert code put here
 headers = {"apikey": apikey, "appName": appName, "sysName": sysName, "cert": cert}
+host = "https://api.imjustgood.com/lineqr"
 
-media = imjustgood(apikey)
-data = media.lineqr(appname)
-timeour = 10
-
-qr_link = data["result"]["qr"]
-print(qr_link)
+data = json.loads(requests.get(host,headers=headers).text)
+print(data["result"]["qr"])
 if cert is None:
-    for i in range(timeout):
+    for i in range(10):
         main = json.loads(requests.get(data["result"]["callback"]["pin"],headers=headers).text)
         if main["status"] == 200:
             print(main["result"]["pin"])
             break
-        elif i == timeout-1:
+        elif i == 9:
             print(main["message"])
             break
         else:time.sleep(1)
-for i in range(timeout):
+for i in range(10):
     main = json.loads(requests.get(data["result"]["callback"]["token"],headers=headers).text)
     if main["status"] == 200:
         print(main["result"]["token"])
         print(main["result"]["cert"])
         break
-    elif i == timeout-1:
+    elif i == 9:
         print(main["message"])
         break
     else:time.sleep(1)
