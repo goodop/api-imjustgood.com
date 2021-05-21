93af0b7a3e454e94373d242a473edbf9c6406aae
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-05-21 16:53:28 +0700

    Update b64decode.py

--- Example/b64decode.py
+++ Example/b64decode.py
@@ -1,7 +1,8 @@
 import json, requests
 
 code = "VGhlQXV0b2JvdHNDb3Jw" # example base64 code
+headers = {"user-agent": "Mozilla/5.0"}
 host = "https://api.imjustgood.com/base64/code="+code
-data = json.loads(requests.get(host).text)
+data = json.loads(requests.get(host,headers=headers).text)
 result = data["result"]
 print(result)
