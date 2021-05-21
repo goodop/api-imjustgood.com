ffa7f8861886a29bbc2bc6e5d78578129aaa1b4f
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-05-21 17:17:15 +0700

    Update b64encode.py

--- Example/b64encode.py
+++ Example/b64encode.py
@@ -1,7 +1,7 @@
 import json, requests
 
 text = "TheAutobotsCorp" # example text
-headers = {"user-agent": "Mozilla/5.0"}
+headers = {"user-agent": "Justgood/5.0"}
 host = "https://api.imjustgood.com/base64/text="+text
 data = json.loads(requests.get(host,headers=headers).text)
 result = data["result"]
