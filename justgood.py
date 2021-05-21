1a67805caed3f1c7713848cc84b95fadd76897b5
Ang <ang@imjustgood.com>
2021-05-21 17:37:03 +0800

    Update justgood.py

--- justgood.py
+++ justgood.py
@@ -4,7 +4,7 @@ class imjustgood(threading.Thread):
     def __init__(self, apikey):
         super(imjustgood, self).__init__()
         self.host = "https://api.imjustgood.com"
-        self.headers = {"apikey": apikey}
+        self.headers = {"apikey": apikey, "User-Agent":"Mozilla/5.0 (Linux; Android 9; cp3705A)"}
         self.session = requests.Session()
 
     def PrintJson(self, jhon):
