a2ab1f43f375e4dd3baff37d7756073256dfefb6
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-05-21 18:14:25 +0700

    Update line_secondary.py

--- Example/line_secondary.py
+++ Example/line_secondary.py
@@ -3,10 +3,11 @@ import time, json, requests
 # Line Version Info https://api.imjustgood.com/line
 
 apikey = "YOUR_APIKEY_HERE"
+uagent = "JustGood/5.0"
 cert = None # if you have a cert code put here
 appname = "CHROMEOS\t2.4.4\tChromeOS\t88" # example app name
 sysname = "JUSTGOOD" # example system name
-headers = {"apikey": apikey, "appName": appName, "sysName": sysName, "cert": cert}
+headers = {"apikey": apikey, "appName": appName, "sysName": sysName, "cert": cert, "user-agent": uagent}
 host = "https://api.imjustgood.com/lineqr"
 
 data = json.loads(requests.get(host,headers=headers).text)
