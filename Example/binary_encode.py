319273b296ac2842b004a42d3597756f003a9463
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-05-21 17:41:42 +0700

    Update binary_encode.py

--- Example/binary_encode.py
+++ Example/binary_encode.py
@@ -1,7 +1,8 @@
 import json, requests
 
 text = "Imjustgood" # example text
+headers = {"user-agent": "Justgood/5.0"}
 host = "https://api.imjustgood.com/binary/text="+text
-data = json.loads(requests.get(host).text)
+data = json.loads(requests.get(host,headers=headers).text)
 result = data["result"]
 print(result)
