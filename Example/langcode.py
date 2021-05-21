11aa5009c8ecac46aa84c581d8d6da303a2fbe64
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-05-21 18:09:36 +0700

    Update langcode.py

--- Example/langcode.py
+++ Example/langcode.py
@@ -1,8 +1,8 @@
 import json, requests
 
 host = "https://api.imjustgood.com/language/code"
-
-data = json.loads(requests.get(host).text)
+headers = {"user-agent": "JustGood/5.0"}
+data = json.loads(requests.get(host,headers=headers).text)
 result = "Language Code :"
 for a in data:
     result += "\n{}".format(a)
