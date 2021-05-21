3f342142b2a976bfc674c2fd4ca5bbe3bda30f9a
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-05-21 17:40:36 +0700

    Update binary_decode.py

--- Example/binary_decode.py
+++ Example/binary_decode.py
@@ -1,7 +1,8 @@
 import json, requests
 
 digit_code = "01001001011011010110101001110101011100110111010001100111011011110110111101100100" # example digit code
+headers = {"user-agent": "Justgood/5.0"}
 host = "https://api.imjustgood.com/binary/digit="+digit_code
-data = json.loads(requests.get(host).text)
+data = json.loads(requests.get(host,headers=headers).text)
 result = data["result"]
 print(result)
