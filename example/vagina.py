6db2678f4647541c1af089700b9223da982ffe06
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-28 22:28:58 +0700

    Create vagina.py

---
+++ example/vagina.py
@@ -0,0 +1,15 @@
+from justgood import imjustgood
+
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.vagina()
+print(data)
+
+# EXAMPLE GET CERTAIN ATTRIBUTES
+result  = "Vagina : {}".format(data["result"]["vagina"])
+result += "\nDepth : {}".format(data["result"]["depth"])
+result += "\nGrip : {}".format(data["result"]["grip"])
+result += "\nHumidity : {}".format(data["result"]["humidity"])
+result += "\nElasticity : {}".format(data["result"]["elasticity"])
+result += "\nDescription : {}".format(data["result"]["description"])
+result += "\nPicture : {}".format(data["result"]["picture"])
+print(result)
