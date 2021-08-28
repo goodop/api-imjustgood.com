35662fe08db42317c013b6dad7a923339cb81500
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-29 01:10:36 +0700

    Create manga_search.py

---
+++ example/manga_search.py
@@ -0,0 +1,18 @@
+from justgood import imjustgood
+
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.mangaSearch("boruto")
+print(data)
+
+# EXAMPLE GET CERTAIN ATTRIBUTES
+result  = "{}".format(data["result"]["title"])
+result += "\nAuthor : {}".format(data["result"]["author"])
+result += "\nGenre : {}".format(data["result"]["genre"])
+result += "\nRating : {}".format(data["result"]["rating"])
+result += "\nRelease : {}".format(data["result"]["release"])
+result += "\nStatus : {}".format(data["result"]["status"])
+result += "\nType : {}".format(data["result"]["type"])
+result += "\nUpdate : {}".format(data["result"]["updated"])
+result += "\n\nSynopsis :\n{}".format(data["result"]["synopsis"])
+result += "\n\nThumbnail : {}".format(data["result"]["thumbnail"])
+print(result)
