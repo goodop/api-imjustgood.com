e1bce31ad172cb02c3005a3ac4aaedb83555ff40
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-28 22:07:58 +0700

    Create image.py

---
+++ example/image.py
@@ -0,0 +1,11 @@
+from justgood import imjustgood
+
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.image("teacher")
+print(data)
+
+# EXAMPLE GET CERTAIN ATTRIBUTES
+result = "GOOGLE IMAGE RESULT :"
+for i in range(len(data["result"])):
+    result += f"\n{i+1}. {data['result'][i]}\n"
+print(result)
