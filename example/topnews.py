6189c1b6aadb3d87e73678d56b765ec08a070d61
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-28 22:47:07 +0700

    Create topnews.py

---
+++ example/topnews.py
@@ -0,0 +1,16 @@
+from justgood import imjustgood
+
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.topnews()
+print(data)
+
+# EXAMPLE GET CERTAIN ATTRIBUTES
+number = 0
+result = "Top News Daily"
+for a in data["result"]:
+    number += 1
+    result += "\n\n{}. {}".format(number,a["title"])
+    result += "\nSource : {}".format(a["source"])
+    result += "\nAuthor : {}".format(a["author"])
+    result += "\nPage : {}".format(a["url"])
+print(result)
