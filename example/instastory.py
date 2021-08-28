c5b39dc4afbfffe5fcfe96e6b9a3e69f3e9d4bec
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-28 21:49:46 +0700

    Create instastory.py

---
+++ example/instastory.py
@@ -0,0 +1,21 @@
+from justgood import imjustgood
+
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.instastory("the.goperation")
+print(data)
+
+# EXAMPLE GET CERTAIN ATTRIBUTES
+result  = "ID : {}".format(data["result"]["id"])
+result += "\nFullname : {}".format(data["result"]["fullname"])
+result += "\nUsername : {}\n".format(data["result"]["username"])
+result += "\nPicture : {}\n".format(data["result"]["picture"])
+result += "\nProfile : {}\n".format(data["result"]["profile"])
+result += "\n- List stories -"
+number = 0
+for a in data["result"]["stories"]:
+  number += 1
+  result += "\nStories {} : {}".format(number,a["url"])
+  result += "\nCreated {}".format(a["created"])
+  if a["type"] == "video":
+    result += "\Thumbnail : {}".format(a["thumbnail"])
+print(result)
