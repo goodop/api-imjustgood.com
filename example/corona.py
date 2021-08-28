509ba651fa6c95921d263669f7b4215c5a45cbde
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-28 23:26:49 +0700

    Create corona.py

---
+++ example/corona.py
@@ -0,0 +1,18 @@
+from justgood import imjustgood
+
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.corona()
+print(data)
+
+# EXAMPLE GET CERTAIN ATTRIBUTES
+result  = "CORONA VIRUS QUICKCOUNT"
+result += "\n{}".format(data["result"]["date"])
+result += "\n\nWorld"
+result += "\nCase : {}".format(data["result"]["world"]["case"])
+result += "\nFit : {}".format(data["result"]["world"]["fit"])
+result += "\nRip : {}".format(data["result"]["world"]["rip"])
+result += "\n\nIndonesia"
+result += "\nCase : {}".format(data["result"]["indonesia"]["case"])
+result += "\nFit : {}".format(data["result"]["indonesia"]["fit"])
+result += "\nRip : {}".format(data["result"]["indonesia"]["rip"])
+print(result)
