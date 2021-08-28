1d4595a5b3521171922d979c6fcf7ec06e49d002
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-28 23:26:01 +0700

    Create bmkg.py

---
+++ example/bmkg.py
@@ -0,0 +1,19 @@
+from justgood import imjustgood
+
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.bmkg()
+print(data)
+
+# EXAMPLE GET CERTAIN ATTRIBUTES
+result  = "Info Gempa BMKG"
+result += "\nTanggal : {}".format(data["result"]["tanggal"])
+result += "\nPukul : {}".format(data["result"]["pukul"])
+result += "\nLokasi : {}".format(data["result"]["lokasi"])
+result += "\nWilayah : {}".format(data["result"]["wilayah"])
+result += "\nKordinat : {}".format(data["result"]["kordinat"])
+result += "\nKedalaman : {}".format(data["result"]["kedalaman"])
+result += "\nKekuatan : {}".format(data["result"]["kekuatan"])
+result += "\nArahan : {}".format(data["result"]["arahan"])
+result += "\nSaran : {}".format(data["result"]["saran"])
+result += "\nPeta Skema : {}".format(data["result"]["skema"])
+print(result)
