43465842c8265b948d760275b8f772bed14cbdf4
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-28 21:54:29 +0700

    Update twitterdl.py

--- example/twitterdl.py
+++ example/twitterdl.py
@@ -1,6 +1,9 @@
 from justgood import imjustgood
 
-api    = imjustgood("YOUR_APIKEY_HERE")
-data   = api.twitterdl("https://twitter.com/rendytr_/status/1367319659741990912?s=20")
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.twitterdl("https://twitter.com/rendytr_/status/1367319659741990912?s=20")
+print(data)
+
+# EXAMPLE GET CERTAIN ATTRIBUTES
 result = "Video URL : {}".format(data["result"]["videoUrl"])
 print(result)
