a70e8297486232469dfe85cb398dd3667d9cce9b
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-29 00:19:52 +0700

    Create jadian.py

---
+++ example/jadian.py
@@ -0,0 +1,11 @@
+from justgood import imjustgood
+
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.jadian("17-08-1945")
+print(data)
+
+# EXAMPLE GET CERTAIN ATTRIBUTES
+result  = "Date : {}".format(data["result"]["date"])
+result += "\n\nRelated : {}".format(data["result"]["related"])
+result += "\n\nDescription : {}".format(data["result"]["description"])
+print(result)
