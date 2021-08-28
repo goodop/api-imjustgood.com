6a2931839181ec78e7992d2e202f8f1df4982a81
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-28 20:15:52 +0700

    Create joox.py

---
+++ example/joox.py
@@ -0,0 +1,12 @@
+from justgood import imjustgood
+
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.joox("lathi")
+
+result  = "Singer : {}".format(data["result"]["singer"])
+result += "\nTitle : {}".format(data["result"]["title"])
+result += "\nDuration : {}".format(data["result"]["duration"])
+result += "\nSize : {}".format(data["result"]["size"])
+result += "\n\nThumbnail :\n{}".format(data["result"]["thumbnail"])
+result += "\n\nMp3 :\n{}".format(data["result"]["mp3Url"])
+print(result)
