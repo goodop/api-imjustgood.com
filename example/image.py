1a338a7dbeccbb82b23bd9490fd25692eb01a613
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-28 22:08:33 +0700

    Update image.py

--- example/image.py
+++ example/image.py
@@ -7,5 +7,5 @@ print(data)
 # EXAMPLE GET CERTAIN ATTRIBUTES
 result = "GOOGLE IMAGE RESULT :"
 for i in range(len(data["result"])):
-    result += f"\n{i+1}. {data['result'][i]}\n"
+    result += f"\n{i+1}. {data['result'][i]}"
 print(result)
