83416ae306ea20fb4bc0e570c0bc51b8ec591bac
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-28 20:11:18 +0700

    Create youtube.py

---
+++ example/youtube.py
@@ -0,0 +1,14 @@
+from justgood import imjustgood
+
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.youtube("despacito")
+
+result  = "Title : {}".format(data["result"]["title"])
+result += "\nAuthor : {}".format(data["result"]["author"])
+result += "\nDuration : {}".format(data["result"]["duration"])
+result += "\nWatched : {}".format(data["result"]["watched"])
+result += "\n\nThumbnail :\n{}".format(data["result"]["thumbnail"])
+result += "\n\nAudio :\n{}".format(data["result"]["audioUrl"])
+result += "\n\nVideo :\n{}".format(data["result"]["videoUrl"])
+result += "\n\nPage : {}".format(data["result"]["pageUrl"])
+print(result)
