97d9a5107fd007410a2ac2958bdb36f2992e526d
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-28 22:29:45 +0700

    Create tits.py

---
+++ example/tits.py
@@ -0,0 +1,14 @@
+from justgood import imjustgood
+
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.tits()
+print(data)
+
+# EXAMPLE GET CERTAIN ATTRIBUTES
+result  = "Tits : {}".format(data["result"]["tits"])
+result += "\nSize : {}{}".format(data["result"]["size"],data["result"]["cup"])
+result += "\nNipple : {}".format(data["result"]["nipple"])
+result += "\nAerola : {}".format(data["result"]["aerola"])
+result += "\nDescription : {}".format(data["result"]["description"])
+result += "\nPicture : {}".format(data["result"]["picture"])
+print(result)
