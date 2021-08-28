819276cd6babd11fab556a73c6f6a5276b672927
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-29 00:59:53 +0700

    Create cctv_search.py

---
+++ example/cctv_search.py
@@ -0,0 +1,14 @@
+from justgood import imjustgood
+
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.cctvSearch("204")
+print(data)
+
+# EXAMPLE GET CERTAIN ATTRIBUTES
+result  = "CCTV CAMERA INFO"
+result += "\nArea : {}".format(data["result"]["area"])
+result += "\nWilayah : {}".format(data["result"]["wilayah"])
+result += "\n\nThumbnail : {}".format(data["result"]["thumbnail"])
+result += "\n\nVideo : {}".format(data["result"]["video"])
+result += "\n\n{}".format(data["result"]["camera"])
+print(result)
