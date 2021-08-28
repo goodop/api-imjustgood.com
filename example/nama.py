e64ef71a0836150469da45b689eb184f8a517707
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-29 00:46:11 +0700

    Create nama.py

---
+++ example/nama.py
@@ -0,0 +1,11 @@
+from justgood import imjustgood
+
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.nama("soekarno")
+print(data)
+
+# EXAMPLE GET CERTAIN ATTRIBUTES
+result  = "Nama : {}".format(data["result"]["name"])
+result += "\nKarakter : {}".format(data["result"]["definition"])
+result += "\nDeskripsi : {}".format(data["result"]["description"])
+print(result)
