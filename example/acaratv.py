d2466d8080aa1da8df17d866f248df19aa18a81a
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-29 00:56:29 +0700

    Create acaratv.py

---
+++ example/acaratv.py
@@ -0,0 +1,14 @@
+from justgood import imjustgood
+
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.acaratv()
+print(data)
+
+# EXAMPLE GET CERTAIN ATTRIBUTES
+result = "Acara TV"
+for a data["result"]:
+    for b in a:
+        result += "\n\nChannel : {}".format(b)
+        for c in a[b]:
+            result += "\n{}".format(c)
+print(result)
