42f3ec6b763c893a753ee9807da21a3408715f6d
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-28 22:24:37 +0700

    Create kamasutra.py

---
+++ example/kamasutra.py
@@ -0,0 +1,11 @@
+from justgood import imjustgood
+
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.kamasutra()
+print(data)
+
+# EXAMPLE GET CERTAIN ATTRIBUTES
+result  = "Style : {}".format(data["result"]["style"])
+result += "\nDescription :\n{}".format(data["result"]["description"])
+result += "\nPicture : {}".format(data["result"]["thumbnail"])
+print(result)
