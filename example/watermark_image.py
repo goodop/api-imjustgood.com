5fe7079cfa0a7c02e34156b714a26b0d9dd08a30
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-29 03:14:41 +0700

    Update watermark_image.py

--- example/watermark_image.py
+++ example/watermark_image.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
 api    = imjustgood("YOUR_APIKEY_HERE")
-data   = api.watermark_text("https://example.com/image.jpg", "https://example.com/icon.png")
+data   = api.watermark_image("https://example.com/image.jpg", "https://example.com/icon.png")
 result = data["result"]
 print(result)
 
