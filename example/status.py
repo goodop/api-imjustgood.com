e017955a9822c59689e6634a557e7bd1ed703837
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-28 20:11:35 +0700

    Update status.py

--- example/status.py
+++ example/status.py
@@ -3,7 +3,7 @@ from justgood import imjustgood
 api  = imjustgood("YOUR_APIKEY_HERE")
 data = api.status()
 
-result = "ID : {}".format(data["result"]["id"])
+result  = "ID : {}".format(data["result"]["id"])
 result += "\nType : {}".format(data["result"]["type"])
 result += "\nUsage : {}".format(data["result"]["usage"])
 result += "\nExpired : {}".format(data["result"]["expired"])
