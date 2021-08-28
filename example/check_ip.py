e10bcfd67b85bd8a2c9c09d8327d9b03a14b9581
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-29 01:45:34 +0700

    Create check_ip.py

---
+++ example/check_ip.py
@@ -0,0 +1,16 @@
+from justgood import imjustgood
+
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.check_ip("8.8.8.8")
+print(data)
+
+# EXAMPLE GET CERTAIN ATTRIBUTES
+result = "Ip Address : {}".format(data["result"]["ip_address"])
+for a in data["result"]:
+    if a != "ip_address" and a != "languages" and data["result"][a] is not None:
+       result += "\n{} : {}".format(a.title(),data["result"][a])
+languages = "\nLanguage : "
+for b in data["result"]["languages"]:
+    languages += "{}, ".format(b)
+result += languages[:-2]
+print(result)
