9ee0d81ee3872c8d2f369d942dd4991a55311795
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-08-28 21:53:29 +0700

    Create facebookdl.py

---
+++ example/facebookdl.py
@@ -0,0 +1,11 @@
+from justgood import imjustgood
+
+api  = imjustgood("YOUR_APIKEY_HERE")
+data = api.facebookdl("https://fb.watch/407ynrmyQq/")
+print(data)
+
+# EXAMPLE GET CERTAIN ATTRIBUTES
+result  = "Author : {}".format(data["result"]["author"])
+result += "\nCaption : {}".format(data["result"]["caption"])
+result += "\nVideo URL : {}".format(data["result"]["videoUrl"])
+print(result)
