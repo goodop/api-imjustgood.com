ad4074dd05cc17af3bedeac78387e6f30647d026
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 04:50:42 +0700

    Update lyric.py

--- example/lyric.py
+++ example/lyric.py
@@ -1,11 +1,13 @@
 from justgood import imjustgood
 
 api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.lyric("crawling")
+data = api.lyric("faded")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
+image   = data["result"]["image"]
 result += "Title : {}".format(data["result"]["title"])
 result += "\nArtist : {}".format(data["result"]["artist"])
 result += "\n\Lyric :\n{}".format(data["result"]["lyric"])
+print(image)
 print(result)
