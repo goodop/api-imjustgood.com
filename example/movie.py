6dc29ff3051c8c6bc18ea7ac44b8fc2e821d9e2f
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 04:54:43 +0700

    Update movie.py

--- example/movie.py
+++ example/movie.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.movie("avengers")
+api     = imjustgood("YOUR_APIKEY_HERE")
+data    = api.movie("avengers")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
