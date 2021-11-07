c626969959e268ff923fcda3b2b642e1c5d15d00
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 03:39:27 +0700

    Update acaratv.py

--- example/acaratv.py
+++ example/acaratv.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.acaratv()
+api    = imjustgood("YOUR_APIKEY_HERE")
+data   = api.acaratv()
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
