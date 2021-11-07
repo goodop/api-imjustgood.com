2f6e4fc6d9515d8d1a0300e9525e5cafb631bb4c
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 03:52:28 +0700

    Update cinema.py

--- example/cinema.py
+++ example/cinema.py
@@ -1,13 +1,13 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.cinema("garut")
+api         = imjustgood("YOUR_APIKEY_HERE")
+data        = api.cinema("garut")
 print(data)
 
 # EXAMPLE GET CINEMA CITY
-result  = "Cinema 21"
-result += "\n\nCity : {}\n".format(data["result"]["city"])
-number = 0
+result      = "Cinema 21"
+result     += "\n\nCity : {}\n".format(data["result"]["city"])
+number      = 0
 for a in data["result"]["data"]:
     number += 1
     result += "\n{}. {}".format(number,a["cinema"])
@@ -25,7 +25,7 @@ result     += "\n\nNow playing :"
 for a in track["nowPlaying"]:
     number += 1
     result += "\n{}. {}".format(number,a["movie"])
-result += "\n\nStudio Image : {}".format(track["studioImage"])
+result     += "\n\nStudio Image : {}".format(track["studioImage"])
 print(result)
 
 # EXAMPLE MOVIE DETAIL
@@ -39,14 +39,14 @@ track       = list_cinema[select_one-1]
 list_track  = [y for y in track["nowPlaying"]]
 movies      = list_track[select_two-1]
 for x in movies["showtime"]:
-    showtime += "{}, ".format(x)
-result = "{}".format(movies["movie"])
-result += "\n\nPrice : {}".format(movies["price"])
-result += "\n\nShowtime : {}".format(showtime[:-2])
-result += "\n\nGenre : {}".format(movies["genre"])
-result += "\nDuration : {}".format(movies["duration"])
-result += "\nDirector : {}".format(movies["director"])
-result += "\nActor : {}".format(movies["actor"])
-result += "\n\nSynopsis :\n{}".format(movies["synopsis"])
-result += "\n\nPoster : {}".format(movies["poster"])
+  showtime += "{}, ".format(x)
+result      = "{}".format(movies["movie"])
+result     += "\n\nPrice : {}".format(movies["price"])
+result     += "\n\nShowtime : {}".format(showtime[:-2])
+result     += "\n\nGenre : {}".format(movies["genre"])
+result     += "\nDuration : {}".format(movies["duration"])
+result     += "\nDirector : {}".format(movies["director"])
+result     += "\nActor : {}".format(movies["actor"])
+result     += "\n\nSynopsis :\n{}".format(movies["synopsis"])
+result     += "\n\nPoster : {}".format(movies["poster"])
 print(result)
