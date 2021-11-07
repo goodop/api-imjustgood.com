1b083be3992444c3cadffe38868817cccee24b87
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 04:55:33 +0700

    Update nama.py

--- example/nama.py
+++ example/nama.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.nama("soekarno")
+api     = imjustgood("YOUR_APIKEY_HERE")
+data    = api.nama("soekarno")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
