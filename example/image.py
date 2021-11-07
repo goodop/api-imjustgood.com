f3f174ca13670006c1ba0b13ceb663d77f7fed5f
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 03:56:58 +0700

    Update image.py

--- example/image.py
+++ example/image.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.image("teacher")
+api    = imjustgood("YOUR_APIKEY_HERE")
+data   = api.image("teacher")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
