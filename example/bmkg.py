bfebd7bd097ab52e4914afdbd2c510b2703a1c2c
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 03:47:12 +0700

    Update bmkg.py

--- example/bmkg.py
+++ example/bmkg.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.bmkg()
+api     = imjustgood("YOUR_APIKEY_HERE")
+data    = api.bmkg()
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
