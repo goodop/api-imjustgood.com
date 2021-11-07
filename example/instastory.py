7edcababac9287ba8275accde6ea6f5be80b48b6
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 04:06:20 +0700

    Update instastory.py

--- example/instastory.py
+++ example/instastory.py
@@ -1,17 +1,17 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.instastory("the.goperation")
+api       = imjustgood("YOUR_APIKEY_HERE")
+data      = api.instastory("the.goperation")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
-result  = "ID : {}".format(data["result"]["id"])
-result += "\nFullname : {}".format(data["result"]["fullname"])
-result += "\nUsername : {}\n".format(data["result"]["username"])
-result += "\nPicture : {}\n".format(data["result"]["picture"])
-result += "\nProfile : {}\n".format(data["result"]["profile"])
-result += "\n- List stories -"
-number = 0
+result    = "ID : {}".format(data["result"]["id"])
+result   += "\nFullname : {}".format(data["result"]["fullname"])
+result   += "\nUsername : {}\n".format(data["result"]["username"])
+result   += "\nPicture : {}\n".format(data["result"]["picture"])
+result   += "\nProfile : {}\n".format(data["result"]["profile"])
+result   += "\n- List stories -"
+number    = 0
 for a in data["result"]["stories"]:
   number += 1
   result += "\nStories {} : {}".format(number,a["url"])
