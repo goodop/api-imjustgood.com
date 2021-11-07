1e8955cce66b488098d8b4795d3f48a4e7aaf50a
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 03:56:01 +0700

    Update github.py

--- example/github.py
+++ example/github.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.github("rendytr")
+api     = imjustgood("YOUR_APIKEY_HERE")
+data    = api.github("rendytr")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
