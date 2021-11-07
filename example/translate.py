a3b9a8ee3d9f5f38cb13c087dde5df65b9b27d1f
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 06:59:06 +0700

    Update translate.py

--- example/translate.py
+++ example/translate.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.translate("en", "mengagumkan")
+api     = imjustgood("YOUR_APIKEY_HERE")
+data    = api.translate("id", "hasta la vista baby")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
@@ -9,5 +9,5 @@ result  = "Traslated to {}".format(data["result"]["language"])
 result += "\n{}".format(data["result"]["translate"])
 print(result)
 
-# GET MORE LANGUAGE CODE HERE
+# CHECK LANGUAGE CODE HERE
 # https://api.imjustgood.com/language/code
