1f965aee4076edb5f0bc087ab1ac652ef78a9c77
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 04:56:53 +0700

    Update porn.py

--- example/porn.py
+++ example/porn.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.porn("blonde")
+api     = imjustgood("YOUR_APIKEY_HERE")
+data    = api.porn("blonde")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
