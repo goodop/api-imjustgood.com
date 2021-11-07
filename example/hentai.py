875dbb70d9702957467c1294dfcb1e8111e56ca9
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 03:56:32 +0700

    Update hentai.py

--- example/hentai.py
+++ example/hentai.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.hentai()
+api    = imjustgood("YOUR_APIKEY_HERE")
+data   = api.hentai()
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
