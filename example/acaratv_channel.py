85967072da46ba7b64c2b5d81c217306b2564ba7
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 03:43:37 +0700

    Update acaratv_channel.py

--- example/acaratv_channel.py
+++ example/acaratv_channel.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.acaratv_channel("globaltv")
+api    = imjustgood("YOUR_APIKEY_HERE")
+data   = api.acaratv_channel("globaltv")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
