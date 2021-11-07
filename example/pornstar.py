6b65546c4b892849d82e7972679a877a7871168f
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 04:59:02 +0700

    Update pornstar.py

--- example/pornstar.py
+++ example/pornstar.py
@@ -1,21 +1,21 @@
 from justgood import imjustgood
 import random
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.pornstar()
+api         = imjustgood("YOUR_APIKEY_HERE")
+data        = api.pornstar()
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
-stars  = random.choice(data["result"])
-result = "Name : {}".format(stars["pornstar"])
-result += "\nBirth : {}".format(stars["birth"])
-result += "\nGender : {}".format(stars["gender"])
-result += "\nCountry : {}".format(stars["country"])
-result += "\nHeight : {}".format(stars["height"])
+stars       = random.choice(data["result"])
+result      = "Name : {}".format(stars["pornstar"])
+result     += "\nBirth : {}".format(stars["birth"])
+result     += "\nGender : {}".format(stars["gender"])
+result     += "\nCountry : {}".format(stars["country"])
+result     += "\nHeight : {}".format(stars["height"])
 if stars["gender"] == "male":
     result += "\nDick : {}".format(stars["dick"])
 if stars["gender"] == "female":
     result += "\nBreast : {}".format(stars["breast"])
     result += "\nTits : {}".format(stars["tits"])
-result += "\n\nImage URL :\n{}".format(stars["image"])
+result     += "\n\nImage URL :\n{}".format(stars["image"])
 print(result)
