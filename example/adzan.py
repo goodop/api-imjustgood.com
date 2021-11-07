0d27c8fb2f51fc041736cf30608783eaecda0b8c
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 03:44:12 +0700

    Update adzan.py

--- example/adzan.py
+++ example/adzan.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.adzan("palembang")
+api     = imjustgood("YOUR_APIKEY_HERE")
+data    = api.adzan("palembang")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
