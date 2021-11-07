88e1e95b32c120928b1b7144ecf22ae1287786be
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 04:01:17 +0700

    Update instagram.py

--- example/instagram.py
+++ example/instagram.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.instagram("the.autobots_corp")
+api     = imjustgood("YOUR_APIKEY_HERE")
+data    = api.instagram("the.autobots_corp")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
@@ -12,9 +12,12 @@ result += "\nBiography : {}".format(data["result"]["biography"])
 result += "\nWebsite : {}".format(data["result"]["website"])
 result += "\nPrivate : {}".format(data["result"]["private"])
 result += "\nVerified : {}".format(data["result"]["verified"])
-result += "\nPost : {}".format(data["result"]["post"])
+result += "\nBusiness : {}".format(data["result"]["business"]["status"])
+result += "\nCategory : {}".format(data["result"]["business"]["category"])
 result += "\nFollower : {}".format(data["result"]["follower"])
 result += "\nFollowing : {}".format(data["result"]["following"])
+result += "\nPost : {}".format(data["result"]["post"])
+result += "\nHighlights : {}".format(len(data["result"]["highlights"]))
 result += "\n\nPicture :\n{}".format(data["result"]["picture"])
 result += "\n\nProfile :\n{}".format(data["result"]["profile"])
 if data["result"]["lastpost"] != []:
