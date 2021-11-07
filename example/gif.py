b5eec45d6fe16cde6decb3b623acfaae260df3b6
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 03:55:32 +0700

    Update gif.py

--- example/gif.py
+++ example/gif.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.gif("coding")
+api    = imjustgood("YOUR_APIKEY_HERE")
+data   = api.gif("coding")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
