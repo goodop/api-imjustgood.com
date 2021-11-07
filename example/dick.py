21876d211fc359f365c93c27b8b74ecaa5543406
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 03:54:00 +0700

    Update dick.py

--- example/dick.py
+++ example/dick.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.dick()
+api     = imjustgood("YOUR_APIKEY_HERE")
+data    = api.dick()
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
