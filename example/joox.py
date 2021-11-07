8d0a63c17675401e0b20ba7af68a3f95c185bfed
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 04:08:09 +0700

    Update joox.py

--- example/joox.py
+++ example/joox.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.joox("lathi")
+api     = imjustgood("YOUR_APIKEY_HERE")
+data    = api.joox("lathi")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
@@ -10,5 +10,6 @@ result += "\nTitle : {}".format(data["result"]["title"])
 result += "\nDuration : {}".format(data["result"]["duration"])
 result += "\nSize : {}".format(data["result"]["size"])
 result += "\n\nThumbnail :\n{}".format(data["result"]["thumbnail"])
+result += "\n\nM4a :\n{}".format(data["result"]["m4aUrl"])
 result += "\n\nMp3 :\n{}".format(data["result"]["mp3Url"])
 print(result)
