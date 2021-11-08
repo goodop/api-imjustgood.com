7669605824fa5006a1b9d1c09a6d46beebf4be00
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 07:22:59 +0700

    Update zodiac.py

--- example/zodiac.py
+++ example/zodiac.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.zodiac("aries")
+api     = imjustgood("YOUR_APIKEY_HERE")
+data    = api.zodiac("aries")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
@@ -11,8 +11,10 @@ result += "\nDate Range : {}".format(data["result"]["date"])
 result += "\nLucky Color : {}".format(data["result"]["color"])
 result += "\nLucky Time : {}".format(data["result"]["time"])
 result += "\nLucky Number : {}".format(data["result"]["number"])
+result += "\n\nProfile : {}".format(data["result"]["profile"])
 result += "\n\nPublic : {}".format(data["result"]["public"])
 result += "\n\nMoney : {}".format(data["result"]["money"])
+result += "\n\nCareer : {}".format(data["result"]["career"])
 result += "\n\nLove Couple : {}".format(data["result"]["love"]["couple"])
 result += "\n\nLove Single : {}".format(data["result"]["love"]["single"])
 result += "\n\nImage : {}".format(data["result"]["image"])
