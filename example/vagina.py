0d890de87fac4c8ec55a5fc3830dbf0acef1548c
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 07:16:37 +0700

    Update vagina.py

--- example/vagina.py
+++ example/vagina.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.vagina()
+api     = imjustgood("YOUR_APIKEY_HERE")
+data    = api.vagina()
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
