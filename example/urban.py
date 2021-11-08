8f9b56cbcf7b47b7973bd746fc8720d141f84d25
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 07:16:16 +0700

    Update urban.py

--- example/urban.py
+++ example/urban.py
@@ -1,7 +1,7 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.urban("dick head")
+api     = imjustgood("YOUR_APIKEY_HERE")
+data    = api.urban("dick head")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
