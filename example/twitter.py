7d798f559fb72205e5170feb25402e92357cab1d
RendyTR <77181144+RendyTR@users.noreply.github.com>
2021-11-08 07:06:17 +0700

    Update twitter.py

--- example/twitter.py
+++ example/twitter.py
@@ -1,15 +1,18 @@
 from justgood import imjustgood
 
-api  = imjustgood("YOUR_APIKEY_HERE")
-data = api.twitter("rendytr_")
+api     = imjustgood("YOUR_APIKEY_HERE")
+data    = api.twitter("rendytr_")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
-result  = "Username : {}".format(data["result"]["username"])
+result += "\nID : {}".format(data["result"]["id"])
+result += "\nUsername : {}".format(data["result"]["username"])
 result += "\nFullname : {}".format(data["result"]["fullname"])
+result += "\nVerified : {}".format(data["result"]["verified"])
 result += "\nBiography : {}".format(data["result"]["biography"])
 result += "\nTweet : {}".format(data["result"]["tweet"])
-result += "\nFollower : {}".format(data["result"]["follower"])
+result += "\nMedia : {}".format(data["result"]["media"])
+result += "\nFollowers : {}".format(data["result"]["follower"])
 result += "\nFollowing : {}".format(data["result"]["following"])
 result += "\n\nAvatar :\n{}".format(data["result"]["avatar"])
 result += "\n\nBanner :\n{}".format(data["result"]["banner"])
